48c3ab93f45f5f32e538430d1e7ce454afc54bac
yanoo <yanoo8184@gmail.com>
2024-06-09 19:44:46 +0900

    calendar-01 backup

--- src/app/pages/calendar-01/calendar-01.component.ts
+++ src/app/pages/calendar-01/calendar-01.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import dayjs, { Dayjs } from 'dayjs';
 dayjs().format()
@@ -6,11 +7,66 @@ dayjs().format()
 @Component({
   selector: 'app-calendar-01',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './calendar-01.component.html'
 })
 export class Calendar01Component {
 
-  
+  /**
+   * 상단 요일 표기용
+   */
+  dayNames = ['일', '월', '화', '수', '목', '금', '토'];
+
+  /**
+   * 년도, 월, 일을 제어하는 핵심 날짜
+   * 년도, 월이 바뀌어야 할 때마다 coreDate를 변경하고
+   * coreDate의 년도, 월을 기준으로 달력을 그린다
+   */
+  coreDate!: Dayjs;
+
+  /**
+   * 유일하게 coreDate와 다른 Dayjs 인스턴스임
+   * 오늘 날짜로 돌아갈 필요가 있을 때 coreDate에 today 객체의 값으로 덮어 쓰는 목적
+   */
+  today!: Dayjs;
+
+  /**
+   * 화면에 보일 날짜들의 집합
+   */
+  dates: Dayjs[] = [];
+
+
+  constructor() {
+    this.today = dayjs();
+    this.coreDate = dayjs();
+    this.generateMonth();
+  }
+
+  isToday(date: Dayjs): boolean {
+    return date.isSame(this.today, 'year') &&
+           date.isSame(this.today, 'month') &&
+           date.isSame(this.today, 'date');
+  }
+
+/**
+ * @date coreDate month의 첫날을 가져옴(ex -> 2023-07-01)
+ * 상단의 요일과 첫날의 위치를 맞추기 위해 이전달의 마지막 주를 앞에 채워넣음
+ * @nextMonth coreDate 다음 month의 첫날 가져옴(ex -> 2023-08-01)
+ */
+  private generateMonth() {
+    let date = this.coreDate.startOf('month').startOf('week');
+    let nextMonth = this.coreDate.add(1, 'month').startOf('month');
+    let dates = [];
+
+    // 다음 월의 첫 날 이전까지 반복
+    while (date.isBefore(nextMonth)) {
+      dates.push(date);
+      date = date.add(1, 'day');
+    }
+
+    this.dates = dates;
+  }
 
 }
